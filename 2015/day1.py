37c9fe76b1bb7e38285f6b46689237022a94c3ce
Badhreesh <rao@grandperspective.de>
2024-08-25 15:04:31 +0200

    Add 2015 day 1

---
+++ 2015/day1.py
@@ -0,0 +1,20 @@
+from collections import Counter
+
+from utils import Input, get_input
+
+_input = get_input(day=1, _input=Input.GIVEN)
+
+parenthesis_count = Counter(_input)
+
+p1 = parenthesis_count["("] - parenthesis_count[")"]
+print(p1)
+
+floor = 0
+for idx, parenthesis in enumerate(_input):
+    value = 1 if parenthesis == "(" else -1
+    floor += value
+    if floor == -1:
+        p2 = idx + 1
+        break
+
+print(p2)
