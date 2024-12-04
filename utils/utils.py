5a574f2cbb29dbc5952d24f6f30f5563fa9e659b
Badhreesh <rao@grandperspective.de>
2024-12-04 05:58:36 +0100

    Do not upload puzzle inputs

--- utils/utils.py
+++ utils/utils.py
@@ -10,5 +10,4 @@ class Input(Enum):
 def get_input(year: int, day: int, _input: Input) -> str:
     file = "demo-input.txt" if _input == Input.DEMO else "input.txt"
     path = Path.cwd() / f"{year}" / "inputs" / f"{day}" / file
-    print(path.exists())
     return path.read_text()
